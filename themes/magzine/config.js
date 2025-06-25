665b9f68db01cc058a6d5ac7f5fbd5862a12d0f3
li yang <33389346+YooooungLee@users.noreply.github.com>
2025-06-25 15:10:59 +0800

    Update config.js
    更新右上角广告栏部分的宣传语，更新按钮内容。

--- themes/magzine/config.js
+++ themes/magzine/config.js
@@ -3,14 +3,14 @@ const CONFIG = {
   MAGZINE_HOME_BANNER_ENABLE: true, // 首屏右上角的宣传位
   MAGZINE_HOME_BUTTON: true,
   MAGZINE_HOME_BUTTON_URL: '/about',
-  MAGZINE_HOME_BUTTON_TEXT: '了解更多',
+  MAGZINE_HOME_BUTTON_TEXT: '关于我',
 
   MAGZINE_HOME_HIDDEN_CATEGORY: '分享杂文', //不希望在首页展示的文章分类，用英文逗号隔开
 
-  MAGZINE_HOME_TITLE: '立即开创您的在线业务。完全免费。',
+  MAGZINE_HOME_TITLE: '如果你问我摸鱼的时间有什么产出，我会把这个呈现给你',
   MAGZINE_HOME_DESCRIPTION:
-    '借助NotionNext，获得助您开创、经营和扩展业务所需的全部工具和帮助。',
-  MAGZINE_HOME_TIPS: 'AI时代来临，这是属于超级个体的狂欢盛宴！',
+    '如果你无力改变环境，就去努力改变自己吧！',
+  MAGZINE_HOME_TIPS: 'AI时代来临，打造个人IP从未如此简单。',
 
   // 首页底部推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   MAGZINE_RECOMMEND_POST_TAG: '推荐',
